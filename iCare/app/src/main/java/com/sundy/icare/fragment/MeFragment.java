1a222ad56b3c29de9b4f03799ec01c526f0aac88
sundy <308379285@qq.com>
2016-04-20 17:22:13 +0800

    删除百度map.so

--- iCare/app/src/main/java/com/sundy/icare/fragment/MeFragment.java
+++ iCare/app/src/main/java/com/sundy/icare/fragment/MeFragment.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
 import com.androidquery.AQuery;
 import com.facebook.drawee.view.SimpleDraweeView;
